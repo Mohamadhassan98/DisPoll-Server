14c794a18b2b245a7c76bd4097e648f104a9f2cc
Mohamadhassan <emohamadhassan@gmail.com>
2019-06-02 15:34:40 +0430

    Added responses.py

---
+++ Tick_server/responses.py
@@ -0,0 +1,16 @@
+customer_already_exists = {
+    'result': False,
+    'message': 'کاربری با این شماره تلفن قبلاً ثبت‌نام کرده.',
+}
+phone_added_successfully = {
+    'result': True,
+    'message': 'شماره تلفن با موفقیت ثبت شد.',
+}
+customer_signup_successful = {
+    'result': True,
+    'message': 'ثبت‌نام با موفقیت انجام شد.',
+}
+customer_code_incorrect = {
+    'result': False,
+    'message': 'کد وارد شده صحیح نیست. لطفاً دوباره امتحان کنید.',
+}
